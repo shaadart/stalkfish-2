4294a5e0d81f32c0f2e5765384b9f645e888cc0b
shaadart <projectcodedorg@gmail.com>
2025-04-13 12:56:41 +0530

    vercel setup

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 pip install -r requirements.txt
 apt-get install build-essential
-
+apt-get update && apt-get install -y libstdc++6
 # Ensure the Stockfish binary is executable
 chmod +x stockfish
 chmod +x stockfish/stockfish-ubuntu-x86-64-avx512
