751ca6a28d5d8ac8df246a7088d50bf50d8d2370
shaadart <projectcodedorg@gmail.com>
2025-04-13 12:21:22 +0530

    vercel setup

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask
 from src.routes import bp   # import the blueprint from your package
 
-def create_app():
-    app = Flask(__name__)
-    app.register_blueprint(bp)
-    return app
+app = Flask(__name__)  # Define the app variable at the module level
+app.register_blueprint(bp)
 
 # For local dev
 if __name__ == "__main__":
-    create_app().run()
+    app.run()
